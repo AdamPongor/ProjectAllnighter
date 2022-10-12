203394692c31d1112dcc63a4f9ce15aba8b1305d
piewbond <piewbond@gmail.com>
2022-10-12 16:00:46 +0200

    fegyver forgas
    a fegyver az iranynak megfeleloen forog meg egy kis smoothness hianyzik hozza hogy jol nezzen ki

--- ProjectAllnighter/Assets/WeaponParent.cs
+++ ProjectAllnighter/Assets/WeaponParent.cs
@@ -7,6 +7,6 @@ public class WeaponParent : MonoBehaviour
     public Vector2 Direction { get; set; }
 
     private void Update() {
-        transform.right = (Direction - (Vector2)transform.position).normalized;
+        transform.right = Direction;
     }
 }
