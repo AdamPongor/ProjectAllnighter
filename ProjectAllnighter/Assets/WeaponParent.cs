148f5a8a15ac234bf48f4c24a426da99cacc9d2b
piewbond <piewbond@gmail.com>
2022-10-12 16:25:43 +0200

    sword megjelenites update
    a feje mogott eltunik alul latszik

--- ProjectAllnighter/Assets/WeaponParent.cs
+++ ProjectAllnighter/Assets/WeaponParent.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class WeaponParent : MonoBehaviour
 {
     public Vector2 Direction { get; set; }
-
+    public SpriteRenderer characterRenderer;
+    public SpriteRenderer weaponRenderer;
     private void Update() {
         transform.right = Direction;
+
+        if (Direction.y > 0)
+        {
+            weaponRenderer.sortingOrder = characterRenderer.sortingOrder - 1;
+        }
+        else
+        {
+            weaponRenderer.sortingOrder = characterRenderer.sortingOrder + 1;
+        }
+
     }
 }
