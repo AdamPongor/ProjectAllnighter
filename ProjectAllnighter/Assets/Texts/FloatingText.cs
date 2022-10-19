1b4e1752257269243ec4c1d03add027b25836cda
AdamPongor <adam.pongor14@gmail.com>
2022-10-19 15:36:07 +0200

    Floatind text system kezdemény
    sorry a peter szöveg miatt, ha hazaértem bekötöm

---
+++ ProjectAllnighter/Assets/Texts/FloatingText.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    private float TTL = 0.4f;
+    private float floatspeed = 300;
+    private Vector3 direction = new Vector3(0, 1, 0);
+    private float timeElapsed = 0.0f;
+
+    public TextMeshProUGUI textmesh;
+    RectTransform rTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textmesh = GetComponent<TextMeshProUGUI>();
+        rTransform = GetComponent<RectTransform>();
+        textmesh.color = new Color(1, 0, 0, 1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeElapsed += Time.deltaTime;
+        rTransform.position += direction * floatspeed * Time.deltaTime;
+
+        textmesh.color = new Color(1, 0, 0, 1 - timeElapsed / TTL);
+
+        if (timeElapsed > TTL)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
