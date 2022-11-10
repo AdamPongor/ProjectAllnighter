af95bd59910da4421936c54d8619ae3dff36e98f
piewbond <piewbond@gmail.com>
2022-11-10 12:28:12 +0100

    masolhato enemy
    aggro, animation

--- ProjectAllnighter/Assets/Slime.cs
+++ ProjectAllnighter/Assets/Slime.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
-
+    Vector2 lastdir;
     public DetectionZone zone;
-    private float moveSpeed = 60f;
+    private float moveSpeed = 40f;
     Rigidbody2D rb;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
+
+
+        animator.SetFloat("AnimMoveX", lastdir.x);
+        animator.SetFloat("AnimMoveY", lastdir.y);
+        animator.SetBool("isMoving", false);
+
         Collider2D detected = zone.detectedObjs[0];
         if (detected)
         {
+            animator.SetBool("isMoving", true);
             Vector2 dir = (detected.transform.position - transform.position).normalized;
-            rb.AddForce(dir*moveSpeed*Time.deltaTime);
+            rb.AddForce(Time.deltaTime * moveSpeed * dir,ForceMode2D.Impulse);
+            animator.SetFloat("AnimMoveX", dir.x);
+            animator.SetFloat("AnimMoveY", dir.y);
+            lastdir = dir;
+            return;
         }
     }
 
